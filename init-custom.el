6385c59f9661aa6ad90e4f95a87422dec70b474a
Adam Bac <me@adambac.com>
2019-03-30 12:48:05 +0100

    Initial commit

---
+++ init-custom.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ac-auto-show-menu t)
+ '(custom-safe-themes
+   (quote
+    ("732b807b0543855541743429c9979ebfb363e27ec91e82f463c91e68c772f6e3" "a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" default)))
+ '(evil-auto-indent nil)
+ '(evil-cross-lines t)
+ '(evil-move-beyond-eol t)
+ '(evil-move-cursor-back nil)
+ '(package-selected-packages
+   (quote
+    (ace-jump-mode auto-complete material-theme which-key general use-package evil))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ace-jump-face-foreground ((t (:background "black" :foreground "yellow" :weight bold)))))
+
