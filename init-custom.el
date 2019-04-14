98b1c805d95723533ded1fb2109bb637f75dc5b1
Adam Bac <me@adambac.com>
2019-04-14 10:54:34 +0200

    Lots of changes

--- init-custom.el
+++ init-custom.el
@@ -3,22 +3,38 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ac-auto-show-menu t)
+ '(ac-auto-show-menu t t)
+ '(c-default-style
+   (quote
+    ((c-mode . "stroustrup")
+     (c++-mode . "stroustrup")
+     (java-mode . "java")
+     (awk-mode . "awk")
+     (other . "gnu"))))
+ '(company-idle-delay 0)
+ '(company-quickhelp-delay 0.2)
  '(custom-safe-themes
    (quote
-    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "732b807b0543855541743429c9979ebfb363e27ec91e82f463c91e68c772f6e3" "a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" default)))
- '(evil-auto-indent nil)
+    ("04232a0bfc50eac64c12471607090ecac9d7fd2d79e388f8543d1c5439ed81f5" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "732b807b0543855541743429c9979ebfb363e27ec91e82f463c91e68c772f6e3" "a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" default)))
  '(evil-cross-lines t)
  '(evil-disable-insert-state-bindings t)
  '(evil-move-beyond-eol t)
  '(evil-move-cursor-back nil)
  '(package-selected-packages
    (quote
-    (helm-spotify-plus helm-descbinds helm-moccur helm-config helm evil-collection evil-surround ## parinfer cider zeno-theme immaterial-theme solarized-theme geiser aggressive-indent evil-cleverparens lispy rainbow-delimiters magit ace-jump-mode auto-complete material-theme which-key general use-package evil))))
+    (evil-leader elpy ws-butler dtrt-indent function-args zenburn-theme company-c-headers company-quickhelp company company-mode helm-gtags ggtags helm-spotify-plus helm-descbinds helm-moccur helm-config helm evil-collection evil-surround ## parinfer cider zeno-theme immaterial-theme solarized-theme geiser aggressive-indent evil-cleverparens lispy rainbow-delimiters magit ace-jump-mode auto-complete material-theme which-key general use-package evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ace-jump-face-foreground ((t (:background "black" :foreground "yellow" :weight bold)))))
-
+ '(ace-jump-face-foreground ((t (:background "black" :foreground "yellow" :weight bold))))
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "light steel blue"))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "green yellow"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "orange3"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "cyan"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "green"))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "orange"))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "tan"))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "slate gray"))))
+ '(rainbow-delimiters-depth-9-face ((t (:foreground "white")))))
