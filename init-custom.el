f7a0c2dff1c223d70579a96378d31b00a78a20f6
Adam Bac <me@adambac.com>
2019-04-16 19:29:04 +0200

    Use mixed pitch fonts in Org Mode

--- init-custom.el
+++ init-custom.el
@@ -15,7 +15,7 @@
  '(company-quickhelp-delay 0.2)
  '(custom-safe-themes
    (quote
-    ("04232a0bfc50eac64c12471607090ecac9d7fd2d79e388f8543d1c5439ed81f5" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "732b807b0543855541743429c9979ebfb363e27ec91e82f463c91e68c772f6e3" "a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" default)))
+    ("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "04232a0bfc50eac64c12471607090ecac9d7fd2d79e388f8543d1c5439ed81f5" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "732b807b0543855541743429c9979ebfb363e27ec91e82f463c91e68c772f6e3" "a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" default)))
  '(evil-cross-lines t)
  '(evil-disable-insert-state-bindings t)
  '(evil-move-beyond-eol t)
@@ -25,7 +25,7 @@
  '(org-startup-with-inline-images t)
  '(package-selected-packages
    (quote
-    (minions moody org-bullets evil-org ranger evil-leader elpy ws-butler dtrt-indent function-args zenburn-theme company-c-headers company-quickhelp company company-mode helm-gtags ggtags helm-spotify-plus helm-descbinds helm-moccur helm-config helm evil-collection evil-surround ## parinfer cider zeno-theme immaterial-theme solarized-theme geiser aggressive-indent evil-cleverparens lispy rainbow-delimiters magit ace-jump-mode auto-complete material-theme which-key general use-package evil)))
+    (visual-fill-column company-box spacemacs-theme minions moody org-bullets evil-org ranger evil-leader elpy ws-butler dtrt-indent function-args zenburn-theme company-c-headers company-quickhelp company company-mode helm-gtags ggtags helm-spotify-plus helm-descbinds helm-moccur helm-config helm evil-collection evil-surround ## parinfer cider zeno-theme immaterial-theme solarized-theme geiser aggressive-indent evil-cleverparens lispy rainbow-delimiters magit ace-jump-mode auto-complete material-theme which-key general use-package evil)))
  '(ranger-override-dired t)
  '(ranger-override-dired-mode t)
  '(ranger-show-literal nil))
@@ -35,8 +35,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ace-jump-face-foreground ((t (:background "black" :foreground "yellow" :weight bold))))
- '(org-block ((t (:background "default" :foreground "#8bc34a"))))
- '(org-block-begin-line ((t (:background "default" :foreground "#b3e5fc" :box nil))))
- '(org-block-end-line ((t (:background "default" :foreground "#b3e5fc" :box nil))))
- '(org-level-1 ((t (:inherit outline-1 :background "default" :box nil :weight bold :height 1.3))))
- '(org-level-2 ((t (:inherit outline-2 :background "default" :box nil :height 1.2)))))
+ '(org-block-begin-line ((t (:background "default" :box nil))))
+ '(org-block-end-line ((t (:background "default" :box nil))))
+ '(org-level-1 ((t (:background "default" :box nil))))
+ '(org-level-2 ((t (:background "default" :box nil))))
+ '(variable-pitch ((t (:family "MathJax_Main")))))
