65ea0f9ca7569aaa81eb4338b249a0d002bac87f
Adam Bac <adam.bac@protonmail.com>
2020-03-01 00:38:18 +0100

    C

--- init-custom.el
+++ init-custom.el
@@ -4,6 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ac-auto-show-menu t t)
+ '(bookmark-save-flag 1)
  '(c-default-style
    (quote
     ((c-mode . "stroustrup")
@@ -17,10 +18,19 @@
  '(custom-safe-themes
    (quote
     ("450f3382907de50be905ae8a242ecede05ea9b858a8ed3cc8d1fbdf2d57090af" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "04232a0bfc50eac64c12471607090ecac9d7fd2d79e388f8543d1c5439ed81f5" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "732b807b0543855541743429c9979ebfb363e27ec91e82f463c91e68c772f6e3" "a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" default)))
+ '(doc-view-continuous t)
+ '(doc-view-resolution 200)
  '(evil-cross-lines t)
  '(evil-disable-insert-state-bindings t)
+ '(evil-emacs-state-modes
+   (quote
+    (5x5-mode archive-mode bbdb-mode biblio-selection-mode blackbox-mode bookmark-bmenu-mode bookmark-edit-annotation-mode browse-kill-ring-mode bubbles-mode bzr-annotate-mode calc-mode cfw:calendar-mode completion-list-mode Custom-mode custom-theme-choose-mode debugger-mode delicious-search-mode desktop-menu-blist-mode desktop-menu-mode dun-mode dvc-bookmarks-mode dvc-diff-mode dvc-info-buffer-mode dvc-log-buffer-mode dvc-revlist-mode dvc-revlog-mode dvc-status-mode dvc-tips-mode ediff-mode ediff-meta-mode efs-mode Electric-buffer-menu-mode emms-browser-mode emms-mark-mode emms-metaplaylist-mode emms-playlist-mode ess-help-mode etags-select-mode fj-mode gc-issues-mode gdb-breakpoints-mode gdb-disassembly-mode gdb-frames-mode gdb-locals-mode gdb-memory-mode gdb-registers-mode gdb-threads-mode gist-list-mode git-commit-mode git-rebase-mode gnus-article-mode gnus-browse-mode gnus-group-mode gnus-server-mode gnus-summary-mode gomoku-mode google-maps-static-mode ibuffer-mode jde-javadoc-checker-report-mode magit-cherry-mode magit-diff-mode magit-log-mode magit-log-select-mode magit-popup-mode magit-popup-sequence-mode magit-process-mode magit-reflog-mode magit-refs-mode magit-revision-mode magit-stash-mode magit-stashes-mode magit-status-mode magit-mode magit-branch-manager-mode magit-commit-mode magit-key-mode magit-rebase-mode magit-wazzup-mode mh-folder-mode monky-mode mpuz-mode mu4e-main-mode mu4e-headers-mode mu4e-view-mode notmuch-hello-mode notmuch-search-mode notmuch-show-mode notmuch-tree-mode occur-mode package-menu-mode pdf-outline-buffer-mode pdf-view-mode proced-mode rcirc-mode rebase-mode recentf-dialog-mode reftex-select-bib-mode reftex-select-label-mode reftex-toc-mode sldb-mode slime-inspector-mode slime-thread-control-mode slime-xref-mode snake-mode solitaire-mode sr-buttons-mode sr-mode sr-tree-mode sr-virtual-mode tar-mode tla-annotate-mode tla-archive-list-mode tla-bconfig-mode tla-bookmarks-mode tla-branch-list-mode tla-browse-mode tla-category-list-mode tla-changelog-mode tla-follow-symlinks-mode tla-inventory-file-mode tla-inventory-mode tla-lint-mode tla-logs-mode tla-revision-list-mode tla-revlog-mode tla-tree-lint-mode tla-version-list-mode twittering-mode urlview-mode vc-annotate-mode vc-dir-mode vc-git-log-view-mode vc-hg-log-view-mode vc-svn-log-view-mode vm-mode vm-summary-mode w3m-mode wab-compilation-mode xgit-annotate-mode xgit-changelog-mode xgit-diff-mode xgit-revlog-mode xhg-annotate-mode xhg-log-mode xhg-mode xhg-mq-mode xhg-mq-sub-mode xhg-status-extra-mode)))
  '(evil-move-beyond-eol t)
  '(evil-move-cursor-back nil)
+ '(helm-apropos-fuzzy-match t)
+ '(helm-imenu-fuzzy-match t)
+ '(helm-lisp-fuzzy-completion t)
+ '(helm-semantic-fuzzy-match t)
  '(org-M-RET-may-split-line (quote ((default))))
  '(org-agenda-files (quote ("~/o")))
  '(org-list-allow-alphabetical t)
@@ -31,7 +41,7 @@
  '(org-startup-with-latex-preview t)
  '(package-selected-packages
    (quote
-    (tuareg visual-fill-mode yasnippet workgroups twilight-theme company-web minimal-theme emmet-mode web-mode visual-fill-column company-box spacemacs-theme minions moody org-bullets evil-org ranger evil-leader elpy ws-butler dtrt-indent function-args zenburn-theme company-c-headers company-quickhelp company company-mode helm-gtags ggtags helm-spotify-plus helm-descbinds helm-moccur helm-config helm evil-collection evil-surround ## parinfer cider zeno-theme immaterial-theme solarized-theme geiser aggressive-indent evil-cleverparens lispy rainbow-delimiters magit ace-jump-mode auto-complete material-theme which-key general use-package evil)))
+    (auctex tuareg visual-fill-mode yasnippet workgroups twilight-theme company-web minimal-theme emmet-mode web-mode visual-fill-column company-box spacemacs-theme minions moody org-bullets evil-org ranger evil-leader elpy ws-butler dtrt-indent function-args zenburn-theme company-c-headers company-quickhelp company company-mode helm-gtags ggtags helm-spotify-plus helm-descbinds helm-moccur helm-config helm evil-collection evil-surround ## parinfer cider zeno-theme immaterial-theme solarized-theme geiser aggressive-indent evil-cleverparens lispy rainbow-delimiters magit ace-jump-mode auto-complete material-theme which-key general use-package evil)))
  '(ranger-override-dired nil)
  '(ranger-override-dired-mode nil)
  '(ranger-show-literal nil))
@@ -40,9 +50,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ace-jump-face-foreground ((t (:background "black" :foreground "yellow" :weight bold))))
- '(highlight ((t (:background "gray23"))))
- '(org-block-begin-line ((t (:background "default" :box nil))))
- '(org-block-end-line ((t (:background "default" :box nil))))
- '(org-level-1 ((t (:background "default" :box nil))))
- '(org-level-2 ((t (:background "default" :box nil)))))
+ )
